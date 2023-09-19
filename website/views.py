4aa936c44a30303a40d53aacb4ad7bd5d5216cb5
AkshayKumarProdsmiths <akshay@prodsmiths.com>
2023-09-20 00:07:43 +0530

    first commit

--- website/views.py
+++ website/views.py
@@ -13,14 +13,13 @@ class IndexView(generic.ListView):
 
 class linkdinprofile(generic.DetailView):
      model = GeneralInformation
-     template_name = 'website/linkdinprofile.html'  # Create a user_information.html template
-     context_object_name = 'user'  # The name to use in the template for the user object
+     template_name = 'website/linkdinprofile.html'  
+     context_object_name = 'user' 
 
      def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user = self.get_object()  # Get the user object
+        user = self.get_object()  
 
-        # Retrieve data from related models and add it to the context
         context['resources'] = Resources.objects.filter(user=user)
         context['education'] = Education.objects.filter(user=user)
         context['address'] = Address.objects.filter(user=user)
