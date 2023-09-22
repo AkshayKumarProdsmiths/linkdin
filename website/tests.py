27e10878bf01777427bd918a875b2df66beebccd
AkshayKumarProdsmiths <akshay@prodsmiths.com>
2023-09-22 19:15:56 +0530

    Unit test for API

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from website.models import GeneralInformation
+from website.serializers import GeneralInformationSerializer
 
-# Create your tests here.
+class GeneralInformationAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('website:generalinformation-list') 
+
+    def test_get_general_information_list(self):
+        GeneralInformation.objects.create(name="User1", student_at="School1", connections="10",
+                                          profile_language="English", public_profile_url="https://example.com/user1")
+        GeneralInformation.objects.create(name="User2", student_at="School2", connections="20",
+                                          profile_language="Spanish", public_profile_url="https://example.com/user2")
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_data = GeneralInformationSerializer(GeneralInformation.objects.all(), many=True).data
+        self.assertEqual(response.data, expected_data,)
--- website/urls.py
+++ website/urls.py
@@ -2,14 +2,14 @@ from django.urls import path,include
 from rest_framework import routers
 from website.views import *
 
-app_name = "website"
+app_name = 'website'
 
 router = routers.DefaultRouter()
 router.register(r'GeneralInformation', GeneralInformationviewSet)
 
 urlpatterns = [
      
-      path('', include(router.urls)),
+      path('', include(router.urls),name=GeneralInformation),
     # path("", views.IndexView.as_view(), name="index"),
     # #--------linkdin_profile---------
     # path('user/<int:pk>/', views.linkdinprofile.as_view(), name='General-Information'),
